574e5e98cf37accecc33cc4ebd8bbc56c67691c9
Saidou N <nasmadiba@gmail.com>
2018-05-24 13:15:11 +0000

    ajout classe teste

---
+++ Test du source control/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  Test du source control
+//
+//  Created by mac on 24/05/2018.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import Foundation
+class Test {
+    func salut()  {
+    print("Salut")
+    }
+}
